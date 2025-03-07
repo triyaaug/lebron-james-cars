02aaee41201142e420407945c6fe786cfc669677
KingMunching <Kinglardizabal02@gmail.com>
2025-03-06 20:15:35 -0500

    Initial commit

---
+++ Lebron-James-Cars/src/test/java/com/example/Lebron_James/_Cars/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Lebron_James._Cars;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
